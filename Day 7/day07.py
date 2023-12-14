ed961c50617680cb35702133881042ff7047750f
Naman Verma <naman.verma26@outlook.com>
2023-12-14 22:24:55 +0530

    idk man

--- Day 7/day07.py
+++ Day 7/day07.py
@@ -1,22 +1,26 @@
 import re
 import sys
-import collections  
+from collections import defaultdict, Counter
 from functools import cmp_to_key
 
-def rankEval(x, y):
+def getRank(numUnique, handValues):
+    handRank = 1
+    if numUnique == 1:
+        handRank = 7                # five of kind
+    elif numUnique == 2:
+        if 4 == handValues:
+            handRank = 6            # four of a kind
+        else:
+            handRank = 5            # full house
+    elif numUnique == 3:
+        if 3 == handValues:
+            handRank = 4            # thee of a kind
+        else:
+            handRank = 3            # two pair
+    else:
+        handRank = 6 - numUnique    # One pair and High Card
     
-    for i in range(len(x)):
-        a = vals[x[i]] if x[i] in vals else int(x[i])
-        b = vals[y[i]] if y[i] in vals else int(y[i])
-
-        # vals.get(x[i], int(x[i]))          idk why this doesnt work fml
-        # b = vals.get(y[i], int(y[i])
-        if a > b:
-            return x
-        elif b > a:
-            return y
-
-    return x
+    return handRank
 
 
 data = open(sys.argv[1]).read().strip() 
@@ -25,38 +29,31 @@ lines= data.split('\n')
 vals = {'T':10, 'J': 11, 'Q': 12, 'K':13, 'A': 14}
 aggregate = []
 
-bids = []
-
 for line in lines: 
     cards, bid = line.split()
-    bids.append(bid)
 
     hand = {}
-
+    enum = 0
     for card in cards:
         hand[card] = hand.get(card,0) + 1
-
-    numUnique = len(hand)
-    if numUnique == 1:
-        handRank = 7                # five of kind
-    elif numUnique == 2:
-        if 4 in hand.values():
-            handRank = 6            # four of a kind
+        if card in vals:
+            enum = enum*10 + vals[card]
         else:
-            handRank = 5            # full house
-    elif numUnique == 3:
-        if 3 in hand.values():
-            handRank = 4            # thee of a kind
-        else:
-            handRank = 3            # two pair
-    else:
-        handRank = 6 - numUnique    # One pair and High Card
-    
-    aggregate.append(str(handRank) + cards)
- 
-ranks = sorted(aggregate, key = lambda x: (rankEval(aggregate[0], aggregate[1])))
- 
-print(aggregate)
+            enum = enum*10 + int(card)
 
+    # print(max(Counter(cards).values()))
+
+    handRank = getRank(len(hand), max(hand.values()))
+    enum = handRank * (10**5) + enum 
+
+    aggregate.append((enum,int(bid)))
     
+sorted = sorted(aggregate, key = lambda x: x[0])
+
+winnings = 0
+
+for i, hand in enumerate(sorted):
+    winnings += (i + 1) * hand[1]
+
+print(winnings)
     
